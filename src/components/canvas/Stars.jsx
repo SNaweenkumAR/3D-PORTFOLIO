671d70efb02a2a8b617a8f21116e9a8ab2f2402f
SNaweenkumAR <naween1924@gmail.com>
2025-01-08 23:52:05 +0530

    commit

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,17 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+
+  const [sphere] = useState(() => {
+    const data = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+    if (data.some((value) => isNaN(value))) {
+      console.error("Invalid geometry data detected in random.inSphere");
+    }
+    return data;
+  });
+   
+
+  // const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 15;
